33496ed4b33ad79c3a2c95a93acad0dea35e2ec4
Will Fritts <waf515@gmail.com>
2019-06-08 12:17:11 -0700

    tests pass linter

--- linter/src/test/java/linter/testlab3.java
+++ linter/src/test/java/linter/testlab3.java
@@ -7,7 +7,13 @@ public class testlab3 {
     @Test
     public void testFindErrors(){
         lab3 classUnderTest = new lab3();
-        assertTrue("function should return error message with line and error",
-                classUnderTest.findErrors());
+        assertEquals("function should return error message with line and error", 46,
+                classUnderTest.findErrors("src/main/resources/gates.js"));
+    }
+
+    @Test
+    public void testFindErrors_wrongFilePath(){
+        lab3 classUnderTest = new lab3();
+        assertEquals("should return -1 for wrong path", -1, classUnderTest.findErrors("test"));
     }
 }
